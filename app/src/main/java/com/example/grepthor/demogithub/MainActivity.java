33594b1754a6986b3ed83e899d4a9e5d51f45f6e
shahnawazahmed88 <shahnawazahmed88@gmail.com>
2018-06-04 16:39:17 +0530

    First

---
+++ app/src/main/java/com/example/grepthor/demogithub/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.grepthor.demogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Hello Git
+        setContentView(R.layout.activity_main);
+    }
+}
